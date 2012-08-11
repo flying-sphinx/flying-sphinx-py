1479787f5f539aa03be0077630502b1b6142774a
Pat Allan <pat@freelancing-gods.com>
2012-08-11 22:35:08 +0100

    Constants (well, close enough)

--- flyingsphinx/api.py
+++ flyingsphinx/api.py
@@ -3,19 +3,22 @@ import os
 import flyingsphinx
 
 class API(object):
+  URI_BASE   = 'https://flying-sphinx.com/api/my/app/'
+  API_ACCEPT = 'application/vnd.flying-sphinx-v3+json'
+
   def __init__(self, identifier = None, api_key = None):
     self.identifier = identifier or os.environ['FLYING_SPHINX_IDENTIFIER']
     self.api_key    = api_key    or os.environ['FLYING_SPHINX_API_KEY']
 
   def post(self, path, body={}):
     requests.post(
-      'https://flying-sphinx.com/api/my/app/' + path,
+      API.URI_BASE + path,
       body, self._headers()
     )
 
   def _headers(self):
     return {
-      'Accept':                  'application/vnd.flying-sphinx-v3+json',
+      'Accept':                  API.API_ACCEPT,
       'X-Flying-Sphinx-Token':   ('%s:%s' % (self.identifier, self.api_key)),
       'X-Flying-Sphinx-Version': ('%s+python' % flyingsphinx.__version__)
     }
