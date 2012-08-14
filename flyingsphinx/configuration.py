3b91b82ce1203cba9ae9999893020d6c2e46897d
Pat Allan <pat@freelancing-gods.com>
2012-08-14 14:33:21 +0100

    Uploading of setting files.

--- flyingsphinx/configuration.py
+++ flyingsphinx/configuration.py
@@ -1,3 +1,5 @@
+from os.path import basename
+
 class Configuration(object):
   def __init__(self, api):
     self.api = api
@@ -6,6 +8,16 @@ class Configuration(object):
     self.api.put('/', {'configuration': configuration})
 
   def upload_from_file(self, path):
-    file     = open(path)
-    contents = file.read()
-    self.upload(contents)
+    file = open(path)
+    self.upload(file.read())
+
+  def upload_settings(self, setting, name, contents):
+    self.api.post('/add_file', {
+      'setting':   setting,
+      'file_name': name,
+      'content':   contents
+    })
+
+  def upload_settings_from_file(self, setting, path):
+    file = open(path)
+    self.upload_settings(setting, basename(file.name), file.read())
--- spec/unit/configuration_spec.py
+++ spec/unit/configuration_spec.py
@@ -21,3 +21,25 @@ def test_configuration_upload_from_file(fake_open):
   api.expects('put').with_args('/', {'configuration': 'content in file'})
 
   Configuration(api).upload_from_file('/path/to/file')
+
+@fudge.test
+def test_settings_upload():
+  api = fudge.Fake('API')
+
+  api.expects('post').with_args('/add_file',
+    {'setting': 'wordforms', 'file_name': 'foo.txt', 'content': 'contents'})
+
+  Configuration(api).upload_settings('wordforms', 'foo.txt', 'contents')
+
+@fudge.patch('__builtin__.open')
+def test_settings_upload_from_file(fake_open):
+  api       = fudge.Fake('API')
+  conf_file = fudge.Fake('File')
+  fake_open.is_callable().calls(lambda path: conf_file)
+  conf_file.provides('read').returns('contents')
+  conf_file.name = '/path/to/foo.txt'
+
+  api.expects('post').with_args('/add_file',
+    {'setting': 'wordforms', 'file_name': 'foo.txt', 'content': 'contents'})
+
+  Configuration(api).upload_settings_from_file('wordforms', '/path/to/foo.txt')
